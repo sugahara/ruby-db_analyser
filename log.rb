2f7b5a2fd06ae0267a951c4b259e21da57c52423
Jun Sugahara <j.sghr.jp@gmail.com>
2012-10-30 09:18:14 +0900

    Add old log file delete procedure and confirm message

--- log.rb
+++ log.rb
@@ -3,6 +3,17 @@ module DBAnalyser
   
   class Log < Logger
     def initialize(logdev)
+      unless logdev == STDOUT
+        if File.exist?(logdev)
+          puts "Are you sure to delete old log file?[y,n]"
+          ans = readline(prompt = "", add_hist = false)
+          if ans=="y"
+            File.delete(logdev)
+          else
+            #do nothing
+          end
+        end
+      end
       super(logdev)
       # @logger = File.open(logfile, 'w')
       self.formatter = proc{|severity, datetime, progname, message|
