11f85ed1a7cf6441fa13f7a520dfaa2703c16809
Jun Sugahara <j.sghr.jp@gmail.com>
2012-12-01 17:18:54 +0900

    bugfix for default config

--- client.rb
+++ client.rb
@@ -16,10 +16,16 @@ module DBAnalyser
   end
 
   class GenericClient
-    def initialize(config = config.dup.update(DNAnalyserConfig::Generic))
-      #ActiveRecord::Base.logger = Logger.new(STDOUT)
-      ActiveRecord::Base.establish_connection(config)
-      @tablelist = ActiveRecord::Base.connection.tables
+
+    def initialize(config = {}, default = DBAnalyser::Config::Generic)
+      @config = default.dup.update(config)
+      # ActiveRecord::Base.logger = Logger.new(STDOUT)
+      ActiveRecord::Base.establish_connection(@config)
+      if @config[:tables].first.nil?
+        @tablelist = ActiveRecord::Base.connection.tables
+      else
+        @tablelist = @config[:tables]
+      end
       TargetTable.table_name = @tablelist.first
     end
     
