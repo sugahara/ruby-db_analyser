310c99402bb7658e00a48ccb3de63fc40a36a2c1
Jun Sugahara <j.sghr.jp@gmail.com>
2012-12-03 16:04:07 +0900

    add timespan variable

--- main_timeslot.rb
+++ main_timeslot.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/packet-client.rb')
 require File.expand_path(File.dirname(__FILE__) + '/timeslot-client.rb')
 require File.expand_path(File.dirname(__FILE__) + '/log.rb')
 require 'benchmark'
+require 'time'
 
 puts Benchmark.measure{
   db_host = ARGV[0]
@@ -10,7 +11,9 @@ puts Benchmark.measure{
   db_pass = ARGV[2]
   db_name = ARGV[3]
   delta_t = ARGV[4].to_f
-  tables = ARGV[5...ARGV.size]
+  start_time = Time.parse(ARGV[5])
+  end_time = Time.parse(ARGV[6])
+  tables = ARGV[7...ARGV.size]
 
   # Basic config of ActiveRecord
   config = {
@@ -20,7 +23,8 @@ puts Benchmark.measure{
     :password => db_pass,
     :database => db_name,
     :delta_t => delta_t,
-    :tables => tables
+    :tables => tables,
+    :time => start_time..end_time
   }
 
   # Set conditions to analyse
